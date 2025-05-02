25593e23bc489701443f00574a61deaad22b2881
AlfieMBClark <alfieclark2015@gmail.com>
2025-05-02 13:00:39 +0800

    LinkedListProgress

--- Part1Code/LinkedList/Main.cpp
+++ Part1Code/LinkedList/Main.cpp
@@ -10,6 +10,14 @@ using namespace std;
 using Clock = chrono::high_resolution_clock;
 
 
+void Alfie(){
+    cout << "Merge Sort & Binary Search\n";
+
+    //Question 1
+    cout << "Question 1: Sort by date.\n\n";
+}
+
+
 int main(){
     
     TransactionNode* txHead = loadTransactions("transactions_cleaned.csv");
@@ -20,39 +28,65 @@ int main(){
     }
     cout << "Loaded " << listLength(txHead) << " transactions and "<< listLength(rvHead) << " reviews.\n";
 
-    /*
+    
     //Prints out all the data
-    cout << "\nAll loaded transactions:\n";
-    cout << "CustID | Product | Category | Price | Date | Payment\n";
+    int totalTransactions =0;
+    int totalReviews=0;
+    //cout << "\nAll loaded transactions:\n";
+    //cout << "CustID | Product | Category | Price | Date | Payment\n";
     for (TransactionNode* cur = txHead; cur != nullptr; cur = cur->next) {
-        auto &t = cur->data;
-        cout 
-        << t.customerID << " | "
-        << t.product    << " | "
-        << t.category   << " | "
-        << t.price      << " | "
-        << t.date       << " | "
-        << t.payment    << "\n";
+        auto &t = cur -> data;
+        //cout << t.customerID << "|"<< t.product<< " | "<< t.category<< " | "<< t.price<< " | "<< t.date<< " | "<< t.payment<< "\n";
+        totalTransactions++;
     }
 
     // 4) Print every review
-    cout << "\nAll loaded reviews:\n";
-    cout << "ProdID | CustID | Rating | Text\n";
+    //cout << "\nAll loaded reviews:\n";
+    //cout << "ProdID | CustID | Rating | Text\n";
     for (ReviewNode* cur = rvHead; cur != nullptr; cur = cur->next) {
         auto &r = cur->data;
-        cout 
-        << r.productID  << " | "
-        << r.customerID << " | "
-        << r.rating     << " | "
-        << r.text       << "\n";
+        //cout << r.productID  << " | "<< r.customerID << " | "<< r.rating     << " | "<< r.text << "\n";
+        totalReviews++;
     }
-    */
+    
+    cout << "Total Reviews: "<<totalReviews<<"\tTotal Transactions: "<<totalTransactions;
+
+    int choice;
+
+    do {
+        cout << "\n===== MENU =====\n";
+        cout << "1. Merge Sort + Binary Search\n";
+        cout << "2. Place 2\n";
+        cout << "3. Place 3\n";
+        cout << "4. Place 4\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "\nRunning Merge Sort and Binary Search...\n";
+                Alfie();
+                break;
+            case 2:
+                cout << "\nPlace 2\n";
+                break;
+            case 3:
+                cout << "\nPlace 3\n";
+                break;
+            case 4:
+                cout << "\nPlace 4\n";
+                break;
+            case 5:
+                cout << "\nExiting...\n";
+                break;
+            default:
+                cout << "\nInvalid choice. Please try again.\n";
+        }
+
+    } while (choice != 0);
 
    
-    
-
-    //Question 1
-    //Merge Sort By Date
     
     return 0;
 }
