a605ddf35b7cc3a5b51da0ff429bae23f0212b3e
AlfieMBClark <alfieclark2015@gmail.com>
2025-05-03 15:53:37 +0800

    Removed redundant.cpp

--- Part1Code/LinkedList/Main.cpp
+++ Part1Code/LinkedList/Main.cpp
@@ -11,54 +11,33 @@ using Clock = chrono::high_resolution_clock;
 
 
 void Alfie(){
-    const string transactionsFile = "transactions_cleaned.csv";
-    const string reviewsFile = "reviews_cleaned.csv";
+    cout << "\nMerge Sort & Binary Search\n";
+    Loader loader("transactions_cleaned.csv", "reviews_cleaned.csv");
 
-    TransactionNode* txHead = loadTransactions(transactionsFile);
-    ReviewNode* rvHead = loadReviews(reviewsFile);
-
-    if (!txHead || !rvHead) {
-        std::cerr << "Error loading data\n";
+    if (!loader.loadTrans()||!loader.loadRev()) {
+        cerr << "Failed to load files";
         return;
     }
 
-    int totalTransactions = 0;
-    int totalReviews = 0;
+    // Access and print transaction data
+    // TransactionNode* current = loader.getTransHead();
+    // while (current != nullptr) {
+    //     const Transaction& t = current->data;
 
-    //cout << "All loaded transactions:\n";
-    //cout << "CustID | Product | Category | Price | Date | Payment\n";
-    for (TransactionNode* cur = txHead; cur != nullptr; cur = cur->next) {
-        const auto& t = cur->data;
-        //cout << t.customerID << " | " << t.product << " | "<< t.category << " | " << t.price << " | "<< t.date << " | " << t.paymentMethod << "\n";
-        totalTransactions++;
-    }
+    //     cout << "Customer ID: " << t.customerID
+    //               << ", Product: " << t.product
+    //               << ", Category: " << t.category
+    //               << ", Price: " << t.price
+    //               << ", Date: " << t.date
+    //               << ", Payment Method: " << t.paymentMethod
+    //               << std::endl;
 
-    //cout << "\nAll loaded reviews:\n";
-    //cout << "ProdID | CustID | Rating | Text\n";
-    for (ReviewNode* cur = rvHead; cur != nullptr; cur = cur->next) {
-        const auto& r = cur->data;
-        //cout << r.productID << " | " << r.customerID << " | "<< r.rating << " | " << r.reviewText << "\n";
-        totalReviews++;
-    }
-
-    cout << "\nTotal Reviews: " << totalReviews<< "\tTotal Transactions: " << totalTransactions << "\n";
-
-    //Clena
-    while (txHead) {
-        TransactionNode* temp = txHead;
-        txHead = txHead->next;
-        delete temp;
-    }
-
-    while (rvHead) {
-        ReviewNode* temp = rvHead;
-        rvHead = rvHead->next;
-        delete temp;
-    }
-    cout << "Merge Sort & Binary Search\n";
-
-    //Question 1
+    //     current = current->next;
+    // }
+    
+    // Question 1
     cout << "Question 1: Sort by date.\n\n";
+    
 }
 
 
@@ -67,7 +46,7 @@ int main(){
     int choice;
 
     do {
-        cout << "\n===== MENU =====\n";
+        cout << "\n \tMENU\t\n";
         cout << "1. Merge Sort + Binary Search\n";
         cout << "2. Place 2\n";
         cout << "3. Place 3\n";
